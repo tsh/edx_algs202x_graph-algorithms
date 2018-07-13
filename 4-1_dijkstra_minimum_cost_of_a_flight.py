108e5462945b6c47c24cff102d2981b480118bfe
tsh <dr.tallin@gmail.com>
2018-07-13 18:04:21 +0300

    complete dijkstra

--- 4-1_dijkstra_minimum_cost_of_a_flight.py
+++ 4-1_dijkstra_minimum_cost_of_a_flight.py
@@ -5,8 +5,18 @@ import queue
 
 
 def distance(adj, cost, s, t):
-    #write your code here
-    return -1
+    dist = [float('Inf') for _ in range(len(adj))]
+    dist[s] = 0
+    q = queue.PriorityQueue()
+    for v, w in enumerate(dist):
+        q.put((w, v))
+    while not q.empty():
+        wvert, vert = q.get()
+        for pos, nbr in enumerate(adj[vert]):
+            if dist[nbr] > wvert + cost[vert][pos]:
+                dist[nbr] = wvert + cost[vert][pos]
+                q.put((wvert + cost[vert][pos], nbr))
+    return -1 if dist[t] == float('Inf') else dist[t]
 
 
 if __name__ == '__main__':
